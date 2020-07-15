2ab40fa4b6c72f5b0b1c214c2361313605128a56
Christian <s134801@student.dtu.dk>
2020-07-15 17:02:06 +0200

    backup for PKPP code

--- commands.sh
+++ commands.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 grep -Eh '^using|^import' *.jl */*.jl */*/*.jl | cut -d' ' -f2 | grep -v '\.\.' | tr -d ',:' | cut -d'.' -f1 | sed '/^$/d' | sort | uniq > packages.txt
 cat packages.txt | tr '\n' ' ' | cat - <(echo "")
+
